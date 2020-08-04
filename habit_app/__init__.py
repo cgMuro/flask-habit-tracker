904c92761196d2c37c7ada92eb502f58362fd3ce
Gio <cmurogio@gmail.com>
2020-08-04 12:54:15 +0200

    routes and templates to handle errors added

--- habit_app/__init__.py
+++ habit_app/__init__.py
@@ -7,7 +7,7 @@ from habit_app.config import Config
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = ''
+login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 
 def create_app(config_class=Config):
@@ -22,8 +22,9 @@ def create_app(config_class=Config):
     # Import and then initialize blueprints
     from habit_app.habits import habits
     from habit_app.auth import auth
-
+    from habit_app.errors import errors
     app.register_blueprint(habits)
     app.register_blueprint(auth)
+    app.register_blueprint(errors)
 
     return app
