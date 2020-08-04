03588bd6d3b8ae30966bca3964d6cc4da597581f
Gio <cmurogio@gmail.com>
2020-08-04 12:55:13 +0200

    new secret key

--- habit_app/config.py
+++ habit_app/config.py
@@ -1,6 +1,3 @@
-import os
-
 class  Config:
-    SECRET_KEY = 'tobechanged'
+    SECRET_KEY = '88188653058881cb55a6c0bd3a547f6f' # Created with secrets
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    
